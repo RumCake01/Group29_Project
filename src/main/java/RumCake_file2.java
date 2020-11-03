25367daa3408b9016dae08c8dca7c4303c084b6a
RumCake <mia.hasnutdinova@gmail.com>
2020-11-03 16:14:54 -0500

    RumCake_added - "Monday code review session at 4pm? - Yes or NO?"

--- src/main/java/RumCake_file2.java
+++ src/main/java/RumCake_file2.java
@@ -3,4 +3,6 @@ public class RumCake_file2 {
     public static void main(String[] args) {
         System.out.println("Let's schedule a code review session guys");
     }
+
+
 }
---
+++ src/main/java/RumCake_file3.java
@@ -0,0 +1,5 @@
+public class RumCake_file3 {
+    public static void main(String[] args) {
+        System.out.println("Monday code review session at 4pm? - Yes or NO?");
+    }
+}
