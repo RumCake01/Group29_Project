29d9dc72275485750abce281751823b106cc9831
RumCake <mia.hasnutdinova@gmail.com>
2020-11-03 16:13:06 -0500

    RumCake_added - "Let's schedule a code review session guys"

--- src/main/java/RumCake_file2.java
+++ src/main/java/RumCake_file2.java
@@ -1,6 +1,6 @@
 public class RumCake_file2 {
 
     public static void main(String[] args) {
-        System.out.println("Let's schedule code review session guys");
+        System.out.println("Let's schedule a code review session guys");
     }
 }
